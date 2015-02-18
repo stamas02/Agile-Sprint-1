ad51ee45e19c91f9b4552a2a88deea8f4e5408e7
stamas01 <stamas01@gmail.com>
2015-02-18 10:02:59 +0000

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	sprint_1_aptitude/bin/AndroidDashboardDesign.apk
    	sprint_1_aptitude/bin/classes.dex
    
    	sprint_1_aptitude/bin/classes/com/aptitudeguru/dashboard/Quantitative.class
    	sprint_1_aptitude/bin/resources.ap_
    	sprint_1_aptitude/src/com/aptitudeguru/dashboard/Quantitative.java
    
    First Test
    
    Doesnt do anithing but the file and the testclass is there
    :D

---
+++ sprint_1_aptitude/src/Test/testDb.java
@@ -0,0 +1,31 @@
+package Test;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.test.AndroidTestCase;
+
+import com.aptitudeguru.dashboard.CTable;
+import com.aptitudeguru.dashboard.DatabaseHandler;
+
+import junit.framework.TestCase;
+
+public class testDb extends AndroidTestCase {
+
+	//
+	public testDb(String name) {		
+		super();
+	}
+
+    public void test() {
+	    //List<CTable> tmp = db.getAllC();
+    	
+    	DatabaseHandler db = new DatabaseHandler(this.getContext());
+	    int i = 2;
+	    assertEquals(2, 2);
+	}
+	
+	
+	
+}
